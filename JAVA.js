da2024292cfc4f41e65f4417f6c640bd9ea44ea4
Quintin772 <29quintin.lauro@boiseschools.net>
2026-02-06 09:21:35 -0700

    Update JAVA.js

--- JAVA.js
+++ JAVA.js
@@ -1,1 +1,12 @@
-
+<button onclick="askQuestion('What\'s your favorite Geometry Dash level?')"> Quick question? </button>
+<script src="script.js"></script>
+<script>
+function askQuestion(question) {
+    alert(question);
+}
+function askQuestion(question) {
+    let answer = prompt(question); 
+    if (answer) {
+        alert("Oh, " + answer + " I love that one!");
+    }
+}
